f75395ba9f4c5fad222dc103793434454a261eb7
Krishna Agarwal <79707976+CODSensei@users.noreply.github.com>
2024-07-15 22:34:39 +0530

    Add files via upload

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -18,12 +19,47 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
 
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
+  }
 
 }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
 
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ps', function()
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" , "asm", "bash", "cpp", "html", "css", "javascript", "dart", "go", "java", "json", "kotlin", "python", "ruby", "rust", "typescript", "zig" },
+  sync_install = false,
+  auto_install = true,
+
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,},
+  indent = {enable = true},
+  })
+
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+
